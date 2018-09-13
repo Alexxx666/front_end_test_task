b29410eb1fe220b92c72b8f1aacb431f8315646a
Aleksandr Imaykin <aimaykin@wiley.com>
2018-09-13 16:39:08 +0300

    first mobile version is ready

--- js/main.js
+++ js/main.js
@@ -16,6 +16,11 @@
       item.addEventListener('click', (event) => {
       event.preventDefault();
       document.getElementById(blocks[index]).style.right = '0';
+        document.getElementById('page_top').style.left = '-100%';
+        document.getElementsByClassName('article_main')[0].style.left = '-100%';
+        setTimeout(() => {
+          document.getElementsByClassName('article_main')[0].classList.add('visuallyhidden');
+        }, 500);
     });
   });
   
@@ -23,6 +28,9 @@
       item.addEventListener('click', (event) => {
       event.preventDefault();
       document.getElementById(blocks[index]).style.right = '-100%';
+        document.getElementById('page_top').style.left = '0';
+        document.getElementsByClassName('article_main')[0].style.left = '0';
+        document.getElementsByClassName('article_main')[0].classList.remove('visuallyhidden');
     });
   })
   
