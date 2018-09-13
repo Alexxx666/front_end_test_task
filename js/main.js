b72d8fae10948ee22221aac8c1e708aee0ef5d31
Aleksandr Imaykin <aimaykin@wiley.com>
2018-09-13 18:11:25 +0300

    minor refactoring

--- js/main.js
+++ js/main.js
@@ -1,37 +1,40 @@
 (function () {
   'use strict';
   
-  Array.from(document.getElementsByClassName('hide')).forEach((item) => {
-    item.classList.add('hideVisually');
-  });
-  
-  const blocks = [
+  const menuBlocks = document.getElementsByClassName('menuBlock');
+  const menuLinks = document.getElementsByClassName('sideMenu_link');
+  const backLinks = document.getElementsByClassName('backLink');
+  const blocksIds = [
     'figures',
     'block1',
     'block2',
     'block3'
-  ];
+  ];  
   
-  Array.from(document.getElementsByClassName('sideMenu_link')).forEach((item, index) => {
-      item.addEventListener('click', (event) => {
+  Array.from(menuBlocks).forEach((item) => {
+    item.classList.add('mobileSliding');
+  });
+  
+  Array.from(menuLinks).forEach((item, index) => {
+    item.addEventListener('click', (event) => {
       event.preventDefault();
-      document.getElementById(blocks[index]).style.right = '0';
-        document.getElementById('page_top').style.left = '-100%';
-        document.getElementsByClassName('article_main')[0].style.left = '-100%';
-        setTimeout(() => {
-          document.getElementsByClassName('article_main')[0].classList.add('visuallyhidden');
-        }, 500);
+      document.getElementById(blocksIds[index]).style.right = '0';
+      document.getElementById('page_top').style.left = '-100%';
+      document.getElementsByClassName('article_main')[0].style.left = '-100%';
+      setTimeout(() => {
+        document.getElementsByClassName('article_main')[0].classList.add('visuallyhidden');
+      }, 500);
     });
   });
   
-  Array.from(document.getElementsByClassName('backLink')).forEach((item, index) => {
+  Array.from(backLinks).forEach((item, index) => {
       item.addEventListener('click', (event) => {
       event.preventDefault();
-      document.getElementById(blocks[index]).style.right = '-100%';
-        document.getElementById('page_top').style.left = '0';
-        document.getElementsByClassName('article_main')[0].style.left = '0';
-        document.getElementsByClassName('article_main')[0].classList.remove('visuallyhidden');
+      document.getElementById(blocksIds[index]).style.right = '-100%';
+      document.getElementById('page_top').style.left = '0';
+      document.getElementsByClassName('article_main')[0].style.left = '0';
+      document.getElementsByClassName('article_main')[0].classList.remove('visuallyhidden');
     });
-  })
+  });
   
 }());
